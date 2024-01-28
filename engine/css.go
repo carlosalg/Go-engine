8a3034949e561c64005abe571372f2fedb88e413
carlos <cabetolg98@gmail.com>
2024-01-27 21:18:15 -0500

    finished data structures

--- engine/css.go
+++ engine/css.go
@@ -14,7 +14,34 @@ type Selector struct {
 }
 
 type SimpleSelector struct {
-  TagName 
-  Id
+  TagName Optional[string] 
+  Id Optional[string]
   Class []string
 }
+
+type Declaration struct {
+  name string
+  value Value 
+}
+
+type Value struct{
+  Keyword string
+  Length struct {
+    Value float32
+    Unit Unit 
+  }
+}
+
+type Unit string
+
+const (
+  Px Unit = "px"
+)
+
+type Color struct {
+  r uint8
+  g uint8
+  b uint8
+  a uint8
+}
+ 
